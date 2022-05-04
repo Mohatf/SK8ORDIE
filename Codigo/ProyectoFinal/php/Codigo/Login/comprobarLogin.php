18d0f2808edbe27231159443ba795e26de25c1e2
Mohatf <55502842+Mohatf@users.noreply.github.com>
2022-05-05 00:03:23 +0200

    Update comprobarLogin.php

--- Codigo/ProyectoFinal/php/Codigo/Login/comprobarLogin.php
+++ Codigo/ProyectoFinal/php/Codigo/Login/comprobarLogin.php
@@ -7,45 +7,43 @@ require '../../DAOS/DAOUsuario.php';
 $usuario = $_POST['usuario'];
 $password = $_POST['password'];
 
-echo $usuario;
-echo $password;
   //Nos conectamos a la BD de AWS
 
-//   $conexion=conectar(true);
+  $conexion=conectar(true);
 
-//   //Iniciamos la sesión
+  //Iniciamos la sesión
 
-//   session_start();
+  session_start();
 
-//   //Comprobamos si el usuario y la contraseña existen
+  //Comprobamos si el usuario y la contraseña existen
 
-//   $comprobarDatos= consultaLogin($conexion,$usuario,$password);
+  $comprobarDatos= consultaLogin($conexion,$usuario,$password);
 
-//   // Si existe
+  // Si existe
 
-//   if(mysqli_num_rows($comprobarDatos)!=0){
-//       $fila=mysqli_fetch_assoc($comprobarDatos);
-//       //Y ambos datos coinciden, iniciamos sesión
-//       crearSesion($fila);
+  if(mysqli_num_rows($comprobarDatos)!=0){
+      $fila=mysqli_fetch_assoc($comprobarDatos);
+      //Y ambos datos coinciden, iniciamos sesión
+      crearSesion($fila);
 
-//       header('Location: ../Pagina/home.php');
+      header('Location: ../Pagina/home.php');
 
-//   }
-//   else{
+  }
+  else{
 
-//       //Si no existe 
+      //Si no existe 
 
-//       $comprobarUsuario= validarUsuario($conexion, $usuario);
-//       if(mysqli_num_rows($comprobarUsuario)!=0){
-//           // Comprobamos si el usuario existe, y la contraseña está mal, se le indicará que la contraseña es incorrecta
-//           header('Location: login.php?error=ContraseñaIncorrecta');
+      $comprobarUsuario= validarUsuario($conexion, $usuario);
+      if(mysqli_num_rows($comprobarUsuario)!=0){
+          // Comprobamos si el usuario existe, y la contraseña está mal, se le indicará que la contraseña es incorrecta
+          header('Location: login.php?error=ContraseñaIncorrecta');
 
-//       }else{
-//           // Si no existe el usuario ni la contraseña, se le indicará que el usuario no existe
-//           header('Location: login.php?error=usuarioNoEncontrado');
-//       }
+      }else{
+          // Si no existe el usuario ni la contraseña, se le indicará que el usuario no existe
+          header('Location: login.php?error=usuarioNoEncontrado');
+      }
 
-//   }
+  }
 
 ?>
 
