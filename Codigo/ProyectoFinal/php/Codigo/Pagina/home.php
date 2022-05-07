059a1778e97ff2ab873f305b25e53d5af50cff63
Mohatf <55502842+Mohatf@users.noreply.github.com>
2022-05-07 17:07:21 +0200

    Update home.php

--- Codigo/ProyectoFinal/php/Codigo/Pagina/home.php
+++ Codigo/ProyectoFinal/php/Codigo/Pagina/home.php
@@ -188,7 +188,7 @@ session_start();
 
       <!-- Copyright -->
       <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
-        © 2020 Copyright: Million Bits
+       &copy; SK8 OR DIE
       </div>
       <!-- Copyright -->
     <!-- Footer -->
