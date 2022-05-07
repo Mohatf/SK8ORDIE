56fcc44e0fe7873ec7e9d3a8e63e3d63c7fd6c32
Mohatf <55502842+Mohatf@users.noreply.github.com>
2022-05-07 17:17:30 +0200

    Update

---
+++ Codigo/ProyectoFinal/php/Codigo/Pagina/Footer.php
@@ -0,0 +1,61 @@
+<footer class="bg-dark text-center text-lg-start">
+
+<!-- Grid container -->
+<div class="container p-4">
+  <!--Grid row-->
+
+  <div class="row text-light">
+      <!--Grid column-->
+      <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+      <h5 class="text-uppercase mb-0">Conócenos</h5>
+
+    <p>
+      El objetivo de Million Bits es proporcionar a los amantes de los videojuegos
+      disfrutar de sus titulos favoritos con una calidad excelente. 
+    </p>
+    <p>
+    <a class="mapaDelSitio"href="mapaDelSitio.html">Mapa del sitio</a>
+    </p>
+      </ul>
+    </div>
+    <!--Grid column-->
+    <!--Grid column-->
+    <div class="col-lg-8 col-md-12 mb-4 mb-md-0">
+      <img class="logo" src="../../../imagenes/skateordielogo3.png" width='200' height='200' alt="">
+    </div>
+    <!--Grid column-->
+
+    <!--Grid column-->
+    <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+      <h5 class="text-uppercase">Redes Sociales</h5>
+
+      <ul class="list-unstyled mb-0">
+        <li>
+          <a href="https://twitter.com/" class="text-white"><i class="fab fa-twitter"></i></a>
+        </li>
+        <li>
+          <a href="https://facebook.com/" class="text-white"><i class="fab fa-facebook"></i></a>
+        </li>
+        <li>
+          <a href="https://instagram.com/" class="text-white"><i class="fab fa-instagram"></i></a>
+        </li>
+        <li>
+          <a href="https://linkedin.com/" class="text-white"><i class="fab fa-linkedin-in"></i></a>
+        </li>
+      </ul>
+    </div>
+    <!--Grid column-->
+
+  
+  </div>
+  <!--Grid row-->
+</div>
+<!-- Grid container -->
+
+<!-- Copyright -->
+<div class="text-center p-3 text-light" style="background-color: #23252B">
+ SK8 OR DIE &copy;
+</div>
+<!-- Copyright -->
+<!-- Footer -->
+</footer>
--- Codigo/ProyectoFinal/php/Codigo/Pagina/home.php
+++ Codigo/ProyectoFinal/php/Codigo/Pagina/home.php
@@ -132,67 +132,7 @@ session_start();
       </div>
     </div>
   </section>
-  <footer class="bg-dark text-center text-lg-start">
-
-      <!-- Grid container -->
-      <div class="container p-4">
-        <!--Grid row-->
-      
-        <div class="row text-light">
-            <!--Grid column-->
-            <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
-            <h5 class="text-uppercase mb-0">Conócenos</h5>
-
-          <p>
-            El objetivo de Million Bits es proporcionar a los amantes de los videojuegos
-            disfrutar de sus titulos favoritos con una calidad excelente. 
-          </p>
-          <p>
-          <a class="mapaDelSitio"href="mapaDelSitio.html">Mapa del sitio</a>
-          </p>
-            </ul>
-          </div>
-          <!--Grid column-->
-          <!--Grid column-->
-          <div class="col-lg-8 col-md-12 mb-4 mb-md-0">
-            <img class="logo" src="../../../imagenes/skateordielogo3.png" width='200' height='200' alt="">
-          </div>
-          <!--Grid column-->
-
-          <!--Grid column-->
-          <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
-            <h5 class="text-uppercase">Redes Sociales</h5>
-
-            <ul class="list-unstyled mb-0">
-              <li>
-                <a href="https://twitter.com/" class="text-white"><i class="fab fa-twitter"></i></a>
-              </li>
-              <li>
-                <a href="https://facebook.com/" class="text-white"><i class="fab fa-facebook"></i></a>
-              </li>
-              <li>
-                <a href="https://instagram.com/" class="text-white"><i class="fab fa-instagram"></i></a>
-              </li>
-              <li>
-                <a href="https://linkedin.com/" class="text-white"><i class="fab fa-linkedin-in"></i></a>
-              </li>
-            </ul>
-          </div>
-          <!--Grid column-->
-
-        
-        </div>
-        <!--Grid row-->
-      </div>
-      <!-- Grid container -->
-
-      <!-- Copyright -->
-      <div class="text-center p-3 text-light" style="background-color: #23252B">
-       SK8 OR DIE &copy;
-      </div>
-      <!-- Copyright -->
-    <!-- Footer -->
-  </footer>
+  <?php include 'Footer.php;'?>              
   <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="../../../js/menu.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
