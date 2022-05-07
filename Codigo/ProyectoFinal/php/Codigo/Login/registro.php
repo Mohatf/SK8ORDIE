a4788392eb4490e2ac733196e0882d495196c3a5
Mohatf <55502842+Mohatf@users.noreply.github.com>
2022-05-07 14:36:18 +0200

    Solucion Errores

--- Codigo/ProyectoFinal/php/Codigo/Login/registro.php
+++ Codigo/ProyectoFinal/php/Codigo/Login/registro.php
@@ -70,7 +70,7 @@
                             <input class="form-control" type="text" placeholder="Email" name="email" id="email">
                             <p class="mensaje-error" id="errorEmail"> <i class="fas fa-exclamation-circle"></i> El email no cumple el formato.</p>
                         </div> 
-                        
+
                         //! ESTA MEDIO PUTO ESTE WEE
                         <div class="col-md-12 form-group">
                             <div class="input-group mb-3">
@@ -114,7 +114,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
     
-    <script src="../../../js/registro.js"></script>
+    <!-- <script src="../../../js/registro.js"></script> -->
 
 </body>
 
