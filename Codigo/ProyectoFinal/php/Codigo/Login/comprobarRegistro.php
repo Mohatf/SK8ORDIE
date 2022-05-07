06a6ae04a79b64debb8afd41529cf7b1168c57c8
Mohatf <55502842+Mohatf@users.noreply.github.com>
2022-05-07 15:18:24 +0200

    Actualizacion

--- Codigo/ProyectoFinal/php/Codigo/Login/comprobarRegistro.php
+++ Codigo/ProyectoFinal/php/Codigo/Login/comprobarRegistro.php
@@ -3,8 +3,8 @@
 require '../../DAOS/conectorBD.php';
 require '../../DAOS/DAOUsuario.php';
 
-$conexion=conectar(true);
 session_start();
+$conexion=conectar(true);
 
 $usuario = $_POST['usuario'];
 $nombre = $_POST['nombre'];
@@ -15,55 +15,41 @@ $email = $_POST['email'];
 $telefono = $_POST['telefono'];
 $fotoPerfil=$_FILES["fotoPerfil"];
 $password = $_POST['password'];
-
-if($_FILES['fotoPerfil']['name']  != null){
-  echo "Hay Algo";
-}else{
-  echo "No hay nada";
-}
+$repetirpassword = $_POST['repetirpassword'];
 
 
+$existeUsuario = validarUsuario($conexion, $usuario); 
+$existeDni = comprobarDni($conexion, $dni); 
+$existeEmail = comprobarEmail($conexion, $email); 
 
 
 
 
-
-
-      
-
-
-// $existeUsuario = validarUsuario($conexion, $usuario); 
-// $existeDni = comprobarDni($conexion, $dni); 
-// $existeEmail = comprobarEmail($conexion, $email); 
-
-
-
-
-// if(mysqli_num_rows($existeUsuario)!=0){
+if(mysqli_num_rows($existeUsuario)!=0){
  
-//     header ('Location: registro.php?error=usuarioExiste');
-//     } else if(mysqli_num_rows($existeDni)!=0){
+    header ('Location: registro.php?error=usuarioExiste');
+    } else if(mysqli_num_rows($existeDni)!=0){
     
-//       header ('Location: registro.php?error=dniExiste');
-//     } else if(mysqli_num_rows($existeEmail)!=0){
+      header ('Location: registro.php?error=dniExiste');
+    } else if(mysqli_num_rows($existeEmail)!=0){
     
-//     header ('Location: registro.php?error=emailExiste');
-//   } else {
-//     if($fotoperfil==null){
-//       registroSinFoto($conexion, $nombre, $usuario, $password, $primerapellido, $segundoapellido, $dni, $email, $telefono);
-//     } else{
-//       registro($conexion, $nombre, $usuario, $password, $primerapellido, $segundoapellido, $dni, $email, $telefono, $fotoperfil);
-//     }
-//     $ultimoId = mysqli_insert_id($conexion);
+    header ('Location: registro.php?error=emailExiste');
+  } else {
+    if($_FILES['fotoPerfil']['name']  != null){
+      registro($conexion, $nombre, $usuario, $password, $primerapellido, $segundoapellido, $dni, $email, $telefono, addslashes(file_get_contents($_FILES['fotoPerfil']['tmp_name'])));
+    } else{
+      registroSinFoto($conexion, $nombre, $usuario, $password, $primerapellido, $segundoapellido, $dni, $email, $telefono);
+    }
+    $ultimoId = mysqli_insert_id($conexion);
 
-//     $idCarrito = $ultimoId;
-//     $idUsuarios = $ultimoId;
+    $idCarrito = $ultimoId;
+    $idUsuarios = $ultimoId;
     
-//     crearCarrito($conexion, $idCarrito, $idUsuarios);
+    crearCarrito($conexion, $idCarrito, $idUsuarios);
     
-//     header ("Location: ../Pagina/home.php?user=$idUsuarios");
+    header ("Location: ../Pagina/home.php");
   
-//   }
+  }
 
  
 
