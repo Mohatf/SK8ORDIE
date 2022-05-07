6faae0216477cd18342937bc264390b6180d6f51
Mohatf <55502842+Mohatf@users.noreply.github.com>
2022-05-07 17:18:20 +0200

    Update home.php

--- Codigo/ProyectoFinal/php/Codigo/Pagina/home.php
+++ Codigo/ProyectoFinal/php/Codigo/Pagina/home.php
@@ -132,7 +132,7 @@ session_start();
       </div>
     </div>
   </section>
-  <?php include 'Footer.php;'?>              
+  <?php include 'Footer.php'; ?>              
   <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="../../../js/menu.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
