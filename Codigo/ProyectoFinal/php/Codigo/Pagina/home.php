ba5c01c4a6500d720d9cd5b6ac35332a8f9d4f6b
Mohatf <55502842+Mohatf@users.noreply.github.com>
2022-05-07 16:34:07 +0200

    Update home.php

--- Codigo/ProyectoFinal/php/Codigo/Pagina/home.php
+++ Codigo/ProyectoFinal/php/Codigo/Pagina/home.php
@@ -132,6 +132,67 @@ session_start();
       </div>
     </div>
   </section>
+  <footer class="bg-dark text-center text-lg-start">
+
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!--Grid row-->
+  
+    <div class="row">
+        <!--Grid column-->
+        <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase mb-0">Conócenos</h5>
+
+       <p>
+         El objetivo de Million Bits es proporcionar a los amantes de los videojuegos
+        disfrutar de sus titulos favoritos con una calidad excelente. 
+       </p>
+       <p>
+       <a class="mapaDelSitio"href="mapaDelSitio.html">Mapa del sitio</a>
+       </p>
+        </ul>
+      </div>
+      <!--Grid column-->
+      <!--Grid column-->
+      <div class="col-lg-8 col-md-12 mb-4 mb-md-0">
+        <img class="logo" src="images/LOGO.png" width='200' height='200' alt="">
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Redes Sociales</h5>
+
+        <ul class="list-unstyled mb-0">
+          <li>
+            <a href="https://twitter.com/" class="text-white"><i class="fab fa-twitter"></i></a>
+          </li>
+          <li>
+            <a href="https://facebook.com/" class="text-white"><i class="fab fa-facebook"></i></a>
+          </li>
+          <li>
+            <a href="https://instagram.com/" class="text-white"><i class="fab fa-instagram"></i></a>
+          </li>
+          <li>
+            <a href="https://linkedin.com/" class="text-white"><i class="fab fa-linkedin-in"></i></a>
+          </li>
+        </ul>
+      </div>
+      <!--Grid column-->
+
+    
+    </div>
+    <!--Grid row-->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+    © 2020 Copyright: Million Bits
+  </div>
+  <!-- Copyright -->
+<!-- Footer -->
+  </footer>
   <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="../../../js/menu.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
