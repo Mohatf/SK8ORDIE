62949f28535964d7179634c951221686685608d5
Mohatf <55502842+Mohatf@users.noreply.github.com>
2022-05-08 15:52:17 +0200

    Update updatePassword.php

--- Codigo/ProyectoFinal/php/Codigo/Login/updatePassword.php
+++ Codigo/ProyectoFinal/php/Codigo/Login/updatePassword.php
@@ -9,31 +9,30 @@ $password= $_POST['password'];
 
 
 $validarUsuario= validarUsuario($conexion, $usuario);
-echo "HOLA";
-var_dump($validarUsuario);
 
-// if(mysqli_num_rows($validarUsuario)!=0){
-//     // SI EXISTE CAMBIAMOS LA CONTRASEÑA
-//     $fila=mysqli_fetch_assoc($validarUsuario);
-//     $idUsuario= $fila['idUsuarios'];
-//     updatePassword($conexion,$idUsuario, $password);
 
-//     header('Location: login.php?accion=passwordCambiada');
+if(mysqli_num_rows($validarUsuario)!=0){
+    // SI EXISTE CAMBIAMOS LA CONTRASEÑA
+    $fila=mysqli_fetch_assoc($validarUsuario);
+    $idUsuario= $fila['idUsuarios'];
+    updatePassword($conexion,$idUsuario, $password);
 
-// }
-// else{
-//     //SI NO EXISTE HACEMOS UNA CONSULTA BUSCANDO AL USUARIO 
-//     $validarUsuario2= validarUsuario($conexion, $usuario);
-//     if(mysqli_num_rows($validarUsuario2)!=0){
-//         // SI  EXISTE EL USUARIO ES QUE LA CONTRASEÑA ES INCORRETA  Y LE MANDAMOS UN ERROR
-//     header('Location: nuevaPassword.php?error=ContraseñaIncorrecta');
+    header('Location: login.php?accion=passwordCambiada');
+
+}
+else{
+    //SI NO EXISTE HACEMOS UNA CONSULTA BUSCANDO AL USUARIO 
+    $validarUsuario2= validarUsuario($conexion, $usuario);
+    if(mysqli_num_rows($validarUsuario2)!=0){
+        // SI  EXISTE EL USUARIO ES QUE LA CONTRASEÑA ES INCORRETA  Y LE MANDAMOS UN ERROR
+    header('Location: nuevaPassword.php?error=ContraseñaIncorrecta');
     
-//     }else{
-//     // SI NO EXISTE EL USUARIO ES QUE EL USUARIO NO ESTA REGISTRADO Y LE MANDAMOS UN ERROR
-//     header('Location: nuevaPassword.php?error=usuarioNoEncontrado');
-//     }
+    }else{
+    // SI NO EXISTE EL USUARIO ES QUE EL USUARIO NO ESTA REGISTRADO Y LE MANDAMOS UN ERROR
+    header('Location: nuevaPassword.php?error=usuarioNoEncontrado');
+    }
 
-// }
+}
 
 
 ?>
