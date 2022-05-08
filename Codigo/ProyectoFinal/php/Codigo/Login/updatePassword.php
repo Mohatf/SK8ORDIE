728eecbde3ab25a699ddb13c9b29fad78144561c
Mohatf <55502842+Mohatf@users.noreply.github.com>
2022-05-08 15:49:50 +0200

    Update updatePassword.php

--- Codigo/ProyectoFinal/php/Codigo/Login/updatePassword.php
+++ Codigo/ProyectoFinal/php/Codigo/Login/updatePassword.php
@@ -6,34 +6,34 @@ $conexion=conectar(true);
 
 $usuario= $_POST['usuario'];
 $password= $_POST['password'];
-echo $usuario;
+
 
 $validarUsuario= validarUsuario($conexion, $usuario);
 
+var_dump($validarUsuario);
 
+// if(mysqli_num_rows($validarUsuario)!=0){
+//     // SI EXISTE CAMBIAMOS LA CONTRASEÑA
+//     $fila=mysqli_fetch_assoc($validarUsuario);
+//     $idUsuario= $fila['idUsuarios'];
+//     updatePassword($conexion,$idUsuario, $password);
 
-if(mysqli_num_rows($validarUsuario)!=0){
-    // SI EXISTE CAMBIAMOS LA CONTRASEÑA
-    $fila=mysqli_fetch_assoc($validarUsuario);
-    $idUsuario= $fila['idUsuarios'];
-    updatePassword($conexion,$idUsuario, $password);
+//     header('Location: login.php?accion=passwordCambiada');
 
-    header('Location: login.php?accion=passwordCambiada');
-
-}
-else{
-    //SI NO EXISTE HACEMOS UNA CONSULTA BUSCANDO AL USUARIO 
-    $validarUsuario2= validarUsuario($conexion, $usuario);
-    if(mysqli_num_rows($validarUsuario2)!=0){
-        // SI  EXISTE EL USUARIO ES QUE LA CONTRASEÑA ES INCORRETA  Y LE MANDAMOS UN ERROR
-    header('Location: nuevaPassword.php?error=ContraseñaIncorrecta');
+// }
+// else{
+//     //SI NO EXISTE HACEMOS UNA CONSULTA BUSCANDO AL USUARIO 
+//     $validarUsuario2= validarUsuario($conexion, $usuario);
+//     if(mysqli_num_rows($validarUsuario2)!=0){
+//         // SI  EXISTE EL USUARIO ES QUE LA CONTRASEÑA ES INCORRETA  Y LE MANDAMOS UN ERROR
+//     header('Location: nuevaPassword.php?error=ContraseñaIncorrecta');
     
-    }else{
-    // SI NO EXISTE EL USUARIO ES QUE EL USUARIO NO ESTA REGISTRADO Y LE MANDAMOS UN ERROR
-    header('Location: nuevaPassword.php?error=usuarioNoEncontrado');
-    }
+//     }else{
+//     // SI NO EXISTE EL USUARIO ES QUE EL USUARIO NO ESTA REGISTRADO Y LE MANDAMOS UN ERROR
+//     header('Location: nuevaPassword.php?error=usuarioNoEncontrado');
+//     }
 
-}
+// }
 
 
 ?>
