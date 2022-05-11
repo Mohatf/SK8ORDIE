bb929c0b883e85d3d2ab6a9355f3b479e3c3f17b
Mohatf <55502842+Mohatf@users.noreply.github.com>
2022-05-11 16:09:59 +0200

    Update CarouselMoviles.php

--- Codigo/ProyectoFinal/php/Codigo/Pagina/CarouselMoviles.php
+++ Codigo/ProyectoFinal/php/Codigo/Pagina/CarouselMoviles.php
@@ -9,7 +9,7 @@
             $enlace="DetallesProductoSkateBoardsCompletos.php?id=".$productosRandom['idProductos'];
         ?>
         <div class="carousel-item <?php echo ($i == 0) ? 'active' : '';?>">
-            <span style="font-size:25px; display: flex; justify-content: center; background: #fff; border: none;  color:black"><?php echo $productosRandom['NombreProducto'] ?></span>
+            <span style="font-size:25px; display: flex; justify-content: center; background: #fff; border: none;  color:black; height: 400px;"><?php echo $productosRandom['NombreProducto'] ?></span>
             <span style="display: flex; justify-content: center; background: #fff; border: none;">
             <a href="<?php echo $enlace; ?>"> <img style="height: 400px;" src="data:image/jpeg;base64,<?php echo base64_encode($productosRandom['Imagen']);?>"></a>
             </span>
