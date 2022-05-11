434f60e4f57d93c8c8ee772a7f4594cd79ebfe6b
Mohatf <55502842+Mohatf@users.noreply.github.com>
2022-05-11 15:59:53 +0200

    Update

--- Codigo/ProyectoFinal/php/Codigo/Pagina/CarouselMoviles.php
+++ Codigo/ProyectoFinal/php/Codigo/Pagina/CarouselMoviles.php
@@ -2,7 +2,7 @@
     <div id="carouselExampleControls1" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
         <?php
-        $buscarProductosRandom =  buscarProductosRandom($conexion);
+        $buscarProductosRandom =  buscarProductosRandomMovil($conexion);
         $i = 0;
         $enlace="";
         while($productosRandom = mysqli_fetch_assoc($buscarProductosRandom)){
@@ -21,11 +21,11 @@
     </div>
 
     <button style="background: #fff; border: none;" class="carousel-control-prev rounded" type="button" data-target="#carouselExampleControls1" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="carousel-control-prev-iconMovil" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
     </button>
     <button style="background: #fff; border: none;" class="carousel-control-next rounded" type="button" data-target="#carouselExampleControls1" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="carousel-control-next-iconMovil" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
     </button>
     </div>
