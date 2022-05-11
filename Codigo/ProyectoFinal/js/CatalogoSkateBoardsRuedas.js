454dc6a22d1510623e2be79a79f14243f4aa095c
Mohatf <55502842+Mohatf@users.noreply.github.com>
2022-05-11 21:41:36 +0200

    Update

--- Codigo/ProyectoFinal/js/CatalogoSkateBoardsRuedas.js
+++ Codigo/ProyectoFinal/js/CatalogoSkateBoardsRuedas.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
              
                 if(response=='0'){
                     
-                    $('#listaProductos').html('<p class="productoNoEncontrado">No se han encontrado registros. :( </p>');
+                    $('#listaProductos').html('<p class="productoNoEncontrado">No se han encontrado registros.</p>');
                 }else{
                     
                     $('#listaProductos').html(response);
