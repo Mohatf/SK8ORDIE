adbb8c6c5b742651e41b5da692689578dd520307
Mohatf <55502842+Mohatf@users.noreply.github.com>
2022-05-11 14:25:21 +0200

    Update DAOUsuario.php

--- Codigo/ProyectoFinal/php/DAOS/DAOUsuario.php
+++ Codigo/ProyectoFinal/php/DAOS/DAOUsuario.php
@@ -102,7 +102,7 @@ function registro($conexion, $nombre, $usuario, $password, $primerapellido, $seg
 
 function registroSinFoto($conexion, $nombre, $usuario, $password, $primerapellido, $segundoapellido, $dni, $email, $telefono){
 
-    $consulta = "INSERT INTO usuarios (`Nombre`, `Usuario`, `Password`, `PrimerApellido`, `SegundoApellido`, `Dni`, `Email`, `Telefono`, `Rol`) VALUES ('$nombre', '$usuario', '$password', '$primerapellido', '$segundoapellido', '$dni', '$email', '$telefono', 'Usuario')";
+    $consulta = "INSERT INTO usuarios (`Nombre`, `Usuario`, `Password`, `PrimerApellido`, `SegundoApellido`, `Dni`, `Email`, `Telefono`, `Rol`, `Direccion`,`ComunidadAutonoma`,`Provincia`, `Ciudad`, `CodigoPostal` ) VALUES ('$nombre', '$usuario', '$password', '$primerapellido', '$segundoapellido', '$dni', '$email', '$telefono', 'Usuario','','','','','')";
     $resultado = mysqli_query($conexion, $consulta);
     return $resultado;
 }
